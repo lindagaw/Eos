ad7ee600b4f26c44b52102985368644e0aa9ab36
Ashley Gao <gaoye0718@gmail.com>
2021-07-25 09:23:07 -0400

    WIP

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
                              restore=params.src_encoder_restore)
     src_classifier = init_model(net=LeNetClassifier(),
                                 restore=params.src_classifier_restore)
-    tgt_encoder = init_model(net=LeNetEncoder(),
-                             restore=params.tgt_encoder_restore)
+    src_encoder = init_model(net=LeNetEncoder(),
+                             restore=params.src_encoder_restore)
     critic = init_model(Discriminator(input_dims=params.d_input_dims,
                                       hidden_dims=params.d_hidden_dims,
                                       output_dims=params.d_output_dims),
@@ -34,10 +34,9 @@ if __name__ == '__main__':
     print(">>> Source Classifier <<<")
     print(src_classifier)
 
-    if not (src_encoder.restored and src_classifier.restored and
-            params.src_model_trained):
-        src_encoder, src_classifier = train_src(
-            src_encoder, src_classifier, src_data_loader)
+
+    src_encoder, src_classifier = train_src(
+        src_encoder, src_classifier, src_data_loader)
 
     # eval source model
     print("=== Evaluating classifier for source domain ===")
@@ -46,22 +45,21 @@ if __name__ == '__main__':
     # train target encoder by GAN
     print("=== Training encoder for target domain ===")
     print(">>> Target Encoder <<<")
-    print(tgt_encoder)
+    print(src_encoder)
     print(">>> Critic <<<")
     print(critic)
 
     # init weights of target encoder with those of source encoder
-    if not tgt_encoder.restored:
-        tgt_encoder.load_state_dict(src_encoder.state_dict())
 
-    if not (tgt_encoder.restored and critic.restored and
-            params.tgt_model_trained):
-        tgt_encoder = train_tgt(src_encoder, tgt_encoder, critic,
-                                src_data_loader, tgt_data_loader)
+    src_encoder.load_state_dict(src_encoder.state_dict())
+
+
+    src_encoder = train_tgt(src_encoder, src_encoder, critic,
+                            src_data_loader, tgt_data_loader)
 
     # eval target encoder on test set of target dataset
     print("=== Evaluating classifier for encoded target domain ===")
     print(">>> source only <<<")
     eval_tgt(src_encoder, src_classifier, tgt_data_loader_eval)
     print(">>> domain adaption <<<")
-    eval_tgt(tgt_encoder, src_classifier, tgt_data_loader_eval)
+    eval_tgt(src_encoder, src_classifier, tgt_data_loader_eval)
