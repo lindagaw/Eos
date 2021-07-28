c587001709768c222edc2b272ee8e7223d18a312
Ashley Gao <gaoye0718@gmail.com>
2021-07-28 18:01:08 -0400

    removed commas after lines in progenitor

--- models/progenitor.py
+++ models/progenitor.py
@@ -17,14 +17,14 @@ class Progenitor(nn.Module):
         # 1st conv layer
         # input [1 x 28 x 28]
         # output [20 x 12 x 12]
-        self.conv1 = nn.Conv2d(1, 20, kernel_size=5),
-        self.pool1 = nn.MaxPool2d(kernel_size=2),
+        self.conv1 = nn.Conv2d(1, 20, kernel_size=5)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
         # 2nd conv layer
         # input [20 x 12 x 12]
         # output [50 x 4 x 4]
-        self.conv2 = nn.Conv2d(20, 50, kernel_size=5),
-        self.dropout2 = nn.Dropout2d(),
-        self.pool2 = nn.MaxPool2d(kernel_size=2),
+        self.conv2 = nn.Conv2d(20, 50, kernel_size=5)
+        self.dropout2 = nn.Dropout2d()
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
 
         self.fc1 = nn.Linear(50 * 4 * 4, 500)
         self.fc2 = nn.Linear(500, 10)
