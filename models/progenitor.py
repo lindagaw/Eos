65db274a971ff8e051f1c8a7ae4fda97e22bd148
Ashley Gao <gaoye0718@gmail.com>
2021-07-28 17:57:27 -0400

    WIP

--- models/progenitor.py
+++ models/progenitor.py
@@ -19,22 +19,20 @@ class Progenitor(nn.Module):
         # output [20 x 12 x 12]
         self.conv1 = nn.Conv2d(1, 20, kernel_size=5),
         self.pool1 = nn.MaxPool2d(kernel_size=2),
-        self.relu1 = nn.ReLU(),
         # 2nd conv layer
         # input [20 x 12 x 12]
         # output [50 x 4 x 4]
         self.conv2 = nn.Conv2d(20, 50, kernel_size=5),
         self.dropout2 = nn.Dropout2d(),
         self.pool2 = nn.MaxPool2d(kernel_size=2),
-        self.relu2 = nn.ReLU()
 
         self.fc1 = nn.Linear(50 * 4 * 4, 500)
         self.fc2 = nn.Linear(500, 10)
 
     def forward(self, input):
         """Forward the Progenitor."""
-        conv_out = self.relu1(self.pool1(self.conv1(input)))
-        conv_out = self.relu2(self.pool2(self.dropout2(self.conv2(conv_out))))
+        conv_out = F.relu(self.pool1(self.conv1(input)))
+        conv_out = F.relu(self.pool2(self.dropout2(self.conv2(conv_out))))
 
         feat = self.fc1(conv_out.view(-1, 50 * 4 * 4))
         out = F.dropout(F.relu(feat), training=self.training)
