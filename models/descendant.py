9440151bd17f2a7e29219f720a5d28b1297edbb4
Ashley Gao <gaoye0718@gmail.com>
2021-07-28 18:03:25 -0400

    updated the Successor and Descendant

--- models/descendant.py
+++ models/descendant.py
@@ -11,27 +11,14 @@ class Descendant(nn.Module):
 
         self.restored = False
 
-        self.descendantEncoder = nn.Sequential(
-            # 1st conv layer
-            # input [1 x 28 x 28]
-            # output [20 x 12 x 12]
-            nn.Conv2d(1, 20, kernel_size=5),
-            nn.MaxPool2d(kernel_size=2),
-            nn.ReLU(),
-            # 2nd conv layer
-            # input [20 x 12 x 12]
-            # output [50 x 4 x 4]
-            # nn.Conv2d(20, 50, kernel_size=5),
-            # nn.Dropout2d(),
-            # nn.MaxPool2d(kernel_size=2),
-            # nn.ReLU()
-        )
+        self.conv1 = nn.Conv2d(1, 20, kernel_size=5)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
         # self.fc1_descendant = nn.Linear(50 * 4 * 4, 500)
         # self.fc2_descendant = nn.Linear(500, 10)
 
     def forward(self, input):
         """Forward the Descendant."""
-        conv_out = self.descendantEncoder(input)
+        conv_out = F.relu(self.pool1(self.conv1(input)))
         #feat = self.fc1_descendant(conv_out.view(-1, 50 * 4 * 4))
         #out = F.dropout(F.relu(feat), training=self.training)
         #out = self.fc2_descendant(out)
--- models/successor.py
+++ models/successor.py
@@ -11,29 +11,23 @@ class Successor(nn.Module):
 
         self.restored = False
 
-        self.successorEncoder = nn.Sequential(
-            # 1st conv layer
-            # input [1 x 28 x 28]
-            # output [20 x 12 x 12]
-            nn.Conv2d(1, 20, kernel_size=5),
-            nn.MaxPool2d(kernel_size=2),
-            nn.ReLU(),
-            # 2nd conv layer
-            # input [20 x 12 x 12]
-            # output [50 x 4 x 4]
-            nn.Conv2d(20, 50, kernel_size=5),
-            nn.Dropout2d(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.ReLU()
-        )
+        # 1st conv layer
+        # input [1 x 28 x 28]
+        # output [20 x 12 x 12]
+        self.conv1 = nn.Conv2d(1, 20, kernel_size=5)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        # 2nd conv layer
+        # input [20 x 12 x 12]
+        # output [50 x 4 x 4]
+        self.conv2 = nn.Conv2d(20, 50, kernel_size=5)
+        self.dropout2 = nn.Dropout2d()
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
         # self.fc1_successor = nn.Linear(50 * 4 * 4, 500)
         # self.fc2_successor = nn.Linear(500, 10)
 
     def forward(self, input):
         """Forward the Successor."""
-        conv_out = self.successorEncoder(input)
-        #feat = self.fc1_successor(conv_out.view(-1, 50 * 4 * 4))
-        #out = F.dropout(F.relu(feat), training=self.training)
-        #out = self.fc2_successor(out)
+        conv_out = F.relu(self.pool1(self.conv1(input)))
+        conv_out = F.relu(self.pool2(self.dropout2(self.conv2(conv_out))))
         out = conv_out
         return out
