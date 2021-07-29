3decf2659efe8fb22c26bdff267fb7974289fde7
Ashley Gao <gaoye0718@gmail.com>
2021-07-29 05:30:45 -0400

    WIP

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,6 +4,6 @@ from .k_mnist import get_kmnist
 from .svhn import get_svhn
 
 from .descendant_activations import get_conv_1_activations
-from .successor_activations import get_conv_1_activations
+from .successor_activations import get_conv_2_activations
 
 __all__ = (get_usps, get_mnist, get_kmnist, get_svhn, get_conv_1_activations, get_conv_2_activations)
