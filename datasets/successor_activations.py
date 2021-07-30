b68f8156b38d4a8fc5cdb7700224a84b44e90db2
Ashley Gao <gaoye0718@gmail.com>
2021-07-30 11:18:23 -0400

    WIP

--- datasets/successor_activations.py
+++ datasets/successor_activations.py
@@ -46,8 +46,6 @@ class Duccessor_Activations(data.Dataset):
             torch.save(TensorDataset(xs_train, ys_train), self.root + self.training)
             torch.save(TensorDataset(xs_test, ys_test), self.root + self.testing)
 
-            print(ys_train.shape)
-
             data_set_train = torch.load(self.root + self.training)
             data_set_test = torch.load(self.root + self.testing)
 
