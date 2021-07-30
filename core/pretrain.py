a9aee16075801710474b53186bc6393dca13515e
Ashley Gao <gaoye0718@gmail.com>
2021-07-30 11:41:16 -0400

    WIP

--- core/pretrain.py
+++ core/pretrain.py
@@ -89,12 +89,13 @@ def eval_src(encoder, classifier, data_loader):
         images = make_variable(images, volatile=True)
         labels = make_variable(labels)
 
+        preds = classifier(encoder(images))
+
         print('-------------------------')
         print(preds)
         print(labels)
         print('-------------------------')
 
-        preds = classifier(encoder(images))
         loss += criterion(preds, labels).data
 
         pred_cls = preds.data.max(1)[1]
