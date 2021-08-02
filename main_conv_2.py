8bd32787e7460fb124378ffc2084b26289f4469b
Ashley Gao <gaoye0718@gmail.com>
2021-08-02 13:25:23 -0400

    WIP

--- main_conv_2.py
+++ main_conv_2.py
@@ -1,10 +1,11 @@
 """Main script for ADDA."""
 import pretty_errors
-
 import params
 from core import eval_src, eval_tgt, train_src, train_tgt, train_tgt_classifier
 from core import train_progenitor, eval_progenitor
 from core import eval_tgt_with_probe
+from core import get_distribution, eval_ADDA
+
 from activations import apply_descendant, apply_successor
 
 from models import Discriminator, LeNetClassifier, LeNetEncoder
