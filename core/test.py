05894b81901cb09029c494a7f2dbe39159672ffd
Ashley Gao <gaoye0718@gmail.com>
2021-08-02 10:26:40 -0400

    WIP

--- core/test.py
+++ core/test.py
@@ -167,26 +167,74 @@ def eval_tgt_with_probe(encoder, critic, src_classifier, tgt_classifier, data_lo
     print("Avg Accuracy = {:2%}".format(accuracy_score(y_true=ys_true, y_pred=ys_pred)))
 
 
-def eval_tgt(tgt_encoder, classifier, data_loader):
+def eval_tgt_with_probe(encoder, critic, src_classifier, tgt_classifier, data_loader):
     """Evaluation for target encoder by source classifier on target dataset."""
     # set eval state for Dropout and BN layers
-    tgt_encoder.eval()
-    classifier.eval()
+    encoder.eval()
+    src_classifier.eval()
+    tgt_classifier.eval()
     # init loss and accuracy
-    loss = 0.0
-    acc = 0.0
+    loss = 0
+    acc = 0
+    f1 = 0
+
+    ys_pred = []
+    ys_true = []
     # set loss function
     criterion = nn.CrossEntropyLoss()
+    flag = False
     # evaluate network
     for (images, labels) in data_loader:
         images = make_variable(images, volatile=True)
         labels = make_variable(labels).squeeze_()
-        torch.no_grad()
-        preds = classifier(tgt_encoder(images))
+
+        probeds = critic(encoder(images))
+
+        for image, label, probed in zip(images, labels, probeds):
+            if torch.argmax(probed) == 1:
+                pred = torch.argmax(src_classifier(encoder(torch.unsqueeze(image, 0)))).detach().cpu().numpy()
+            else:
+                pred = torch.argmax(tgt_classifier(encoder(torch.unsqueeze(image, 0)))).detach().cpu().numpy()
+
+        ys_pred.append(np.squeeze(pred))
+        ys_true.append(np.squeeze(label.detach().cpu().numpy()))
+
+    acc = accuracy_score(ys_true, ys_pred)
+
+
+    print("Avg Loss = {}, Accuracy = {:2%}".format(loss, acc))
+
+def eval_tgt(encoder, classifier, data_loader):
+    """Evaluation for target encoder by source classifier on target dataset."""
+    # set eval state for Dropout and BN layers
+    encoder.eval()
+    classifier.eval()
+
+    # init loss and accuracy
+    loss = 0.0
+    acc = 0.0
+
+    ys_true = []
+    ys_pred = []
+
+    # set loss function
+    criterion = nn.CrossEntropyLoss()
+
+    # evaluate network
+    for (images, labels) in data_loader:
+        images = make_variable(images, volatile=True)
+        labels = make_variable(labels).squeeze_()
+
+        preds = classifier(encoder(images))
         loss += criterion(preds, labels).data
 
-        pred_cls = preds.data.max(1)[1]
-        acc += pred_cls.eq(labels.data).cpu().sum()
+        for pred, label in zip(preds, labels):
+            ys_pred.append(torch.argmax(pred).detach().cpu().numpy())
+            ys_true.append(label.detach().cpu().numpy())
+
+    acc = accuracy_score(ys_true, ys_pred)
+
     loss /= len(data_loader)
-    acc /= len(data_loader.dataset)
+    #acc /= len(data_loader.dataset)
+
     print("Avg Loss = {}, Avg Accuracy = {:2%}".format(loss, acc))
