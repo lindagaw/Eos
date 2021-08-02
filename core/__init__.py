d432ea789fc5886ff5bad65057fd7eec39625d27
Ashley Gao <gaoye0718@gmail.com>
2021-08-02 09:54:52 -0400

    WIP

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,9 @@
 from .adapt import train_tgt, train_tgt_classifier
 from .pretrain import eval_src, train_src
-from .test import eval_tgt, eval_tgt_with_probe
+from .test import eval_tgt, eval_tgt_with_probe, get_distribution
 
 from .prepare import train_progenitor, eval_progenitor
 
 __all__ = (eval_src, train_src, train_tgt, eval_tgt, train_tgt_classifier, \
-            eval_tgt_with_probe, \
+            eval_tgt_with_probe, get_distribution,\
             train_progenitor, eval_progenitor)
