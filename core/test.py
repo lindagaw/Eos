c162f11ea79310ed8a77ef614d538d900d102fe3
Ashley Gao <gaoye0718@gmail.com>
2021-08-03 01:31:57 -0400

    WIP

--- core/test.py
+++ core/test.py
@@ -230,16 +230,14 @@ def eval_tgt(encoder, classifier, data_loader):
         preds = classifier(encoder(images))
         loss += criterion(preds, labels).data
 
+        print(labels)
+        print(preds)
+
         for pred, label in zip(preds, labels):
             ys_pred.append(torch.argmax(pred).detach().cpu().numpy())
             ys_true.append(label.detach().cpu().numpy())
 
     acc = accuracy_score(ys_true, ys_pred)
-
-    print('------------')
-    print(ys_true)
-    print(ys_pred)
-    print('-------------')
 
     loss /= len(data_loader)
     #acc /= len(data_loader.dataset)
