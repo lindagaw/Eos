a19462eff06a2b29ab2e0faa476974c758d4a503
Ashley Gao <gaoye0718@gmail.com>
2021-08-04 12:39:38 -0400

    WIP

--- datasets/office_home.py
+++ datasets/office_home.py
@@ -103,7 +103,7 @@ class Office_Home(data.Dataset):
         data_set = torch.load(f)
 
         audios = torch.Tensor([np.asarray(audio) for _, (audio, _) in enumerate(data_set)])
-        labels = torch.Tensor([np.asarray(label) for _, (_, label) in enumerate(data_set)])
+        labels = torch.Tensor([np.asarray([label]) for _, (_, label) in enumerate(data_set)])
 
         self.dataset_size = labels.shape[0]
 
