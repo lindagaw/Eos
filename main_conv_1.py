7e2dfafd6f4dd03d7c9c63d42aa699c87dbed7fc
Ashley Gao <gaoye0718@gmail.com>
2021-08-17 21:49:18 -0400

    WIP

--- main_conv_1.py
+++ main_conv_1.py
@@ -76,10 +76,10 @@ if __name__ == '__main__':
 
     # load models
     src_encoder = torch.nn.Sequential(*(list(progenitor.children())[2:10]))
-    src_classifier = torch.nn.Sequential(*(list(progenitor.children())[10:]))
+    src_classifier = torch.nn.Sequential(*(list(progenitor.children())[11:]))
 
     tgt_classifier = torch.nn.Sequential(*(list(progenitor.children())[2:10]))
-    tgt_encoder = torch.nn.Sequential(*(list(progenitor.children())[10:]))
+    tgt_encoder = torch.nn.Sequential(*(list(progenitor.children())[11:]))
 
     critic = init_model(Discriminator(input_dims=params.d_input_dims,
                                       hidden_dims=params.d_hidden_dims,
