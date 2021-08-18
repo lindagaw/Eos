81888d6d03ca1ee3d41b02aa29a4756f921751ab
Ashley Gao <gaoye0718@gmail.com>
2021-08-18 18:11:24 -0400

    WIP

--- main_conv_1.py
+++ main_conv_1.py
@@ -42,8 +42,8 @@ if __name__ == '__main__':
     tgt_data_loader_eval = get_office_31(dataset = 'office-31-webcam', train=False)
 
 
-    progenitor = models.densenet201(pretrained='imagenet')
-    progenitor.fc = torch.nn.Linear(1920, 31)
+    progenitor = models.googlenet(pretrained=False, aux_logits=False)
+    progenitor.fc = torch.nn.Linear(1024, 31)
     progenitor = progenitor.to(torch.device('cuda:0'))
     #newmodel = torch.nn.Sequential(*(list(model.children())[:-1]))
     print(progenitor)
@@ -75,12 +75,12 @@ if __name__ == '__main__':
     print(">>> train the src_encoder, tgt_encoder, src_classifier, tgt_classifier <<<")
 
     # load models
+    # load models
     src_encoder = torch.nn.Sequential(*(list(progenitor.children())[2:-1]))
-    src_classifier = torch.nn.Linear(1920, 31)
+    src_classifier = torch.nn.Linear(1024, 31)
 
     tgt_classifier = torch.nn.Sequential(*(list(progenitor.children())[2:-1]))
-    tgt_encoder = torch.nn.Linear(1920, 31)
-
+    tgt_encoder = torch.nn.Linear(1024, 31)
     critic = init_model(Discriminator(input_dims=params.d_input_dims,
                                       hidden_dims=params.d_hidden_dims,
                                       output_dims=params.d_output_dims),
