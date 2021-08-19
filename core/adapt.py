ca94846ed7150c2142e001539bc6fae9750c19ed
Ashley Gao <gaoye0718@gmail.com>
2021-08-18 21:18:14 -0400

    WIP

--- core/adapt.py
+++ core/adapt.py
@@ -57,8 +57,7 @@ def train_tgt(src_encoder, tgt_encoder, critic, src_data_loader, tgt_data_loader
             feat_concat = torch.cat((feat_src, feat_tgt), 0)
 
             # predict on discriminator
-            print(feat_concat.shape)
-            pred_concat = critic(feat_concat.detach())
+            pred_concat = critic(torch.squeeze(feat_concat).detach())
 
             # prepare real and fake label
             label_src = make_variable(torch.ones(feat_src.size(0)).long())
