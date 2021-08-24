4e4fcc457b2a65d162131ac6cea2b097d66105e6
Ashley Gao <gaoye0718@gmail.com>
2021-08-24 16:04:20 -0400

    WIP

--- datasets/office_31.py
+++ datasets/office_31.py
@@ -116,7 +116,7 @@ def get_office_31(train, dataset):
                                           mean=params.dataset_mean,
                                           std=params.dataset_std)])
     
-    pre_process =  transforms.Compose([transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]])
+    pre_process =  transforms.Compose([transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]]))
 
     office_31_dataset = Office_31(root=params.data_root,
                         train=train,
