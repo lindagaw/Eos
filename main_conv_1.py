4ca8f41f46c5b8c5beced7ed5d82567f69a29847
Ashley Gao <gaoye0718@gmail.com>
2021-08-27 15:05:51 -0400

    WIP

--- main_conv_1.py
+++ main_conv_1.py
@@ -61,10 +61,10 @@ if __name__ == '__main__':
 
 
     print(">>> get the activations after the nth conv, using Descendant <<<")
-    #apply_descendant(descendant, tgt_data_loader_eval, 'tgt', 'eval')
-    #apply_descendant(descendant, tgt_data_loader, 'tgt', 'dev')
-    #apply_descendant(descendant, src_data_loader, 'src', 'dev')
-    #apply_descendant(descendant, src_data_loader_eval, 'src', 'eval')
+    apply_descendant(descendant, tgt_data_loader_eval, 'tgt', 'eval')
+    apply_descendant(descendant, tgt_data_loader, 'tgt', 'dev')
+    apply_descendant(descendant, src_data_loader, 'src', 'dev')
+    apply_descendant(descendant, src_data_loader_eval, 'src', 'eval')
 
 
     print(">>> construct dataloader after activations from 1st conv <<<")
